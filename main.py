b824bbbe71788d27e0c124c15758a60db04e63d9
matthew-64 <matthewe07@gmail.com>
2019-08-21 23:41:48 +0100

    Refactoring for better design

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from file_reader.csv_reader import InsultReader
 from insult.insult_generator import Insultor
+from meme.meme_generator import MemeGenerator
+import random
+import os
 
-filepath = 'file_reader/insults.csv'
+insults_filepath = './file_reader/insults.csv'
+font_filepath = './meme/fonts/impact/impact.ttf'
+base_image_dir = './meme/pictures/base_images/'
+completed_meme_filepath = './meme/pictures/completed_meme/meme.jpg'
 
-insultReader = InsultReader(filepath)
+base_image_list = os.listdir(base_image_dir)
 
+insultReader = InsultReader(insults_filepath)
 insultor = Insultor(insultReader.get_insults())
+meme_generator = MemeGenerator(completed_meme_filepath, font_filepath)
 
-print(insultor.generate_insult())
-print(insultor.generate_insult())
-print(insultor.generate_insult())
-print(insultor.generate_insult())
-print(insultor.generate_insult())
+meme_generator.generate_meme(
+    base_image_dir + random.choice(os.listdir(base_image_dir)),
+    insultor.generate_insult())
--- meme/meme_generator.py
+++ meme/meme_generator.py
@@ -1,27 +1,28 @@
 from PIL import Image, ImageDraw, ImageFont
 
+text_margin = 0.1
+
 
 class MemeGenerator:
-
-    text_margin = 0.1
 
     def __init__(self, completed_meme_path: str, font_filepath: str):
         self._completed_meme_path = completed_meme_path
         self._font_filepath = font_filepath
 
+    def __calculate_font_size(self, insult: str, image: Image) -> int:
+        font_size = 1
+        font = ImageFont.truetype(self._font_filepath, font_size)
+        while font.getsize(insult)[0] < (1 - text_margin) * image.size[0]:  # put text margin here
+            font_size += 1
+            font = ImageFont.truetype(self._font_filepath, font_size)
+        return font_size
+
     def generate_meme(self, base_image_filepath: str, insult: str) -> None:
         image = Image.open(base_image_filepath)
         draw = ImageDraw.Draw(image)
         image_width, image_height = image.size
-
-        font_size = 1
-        font = ImageFont.truetype(self._font_filepath, font_size)
-        while font.getsize(insult)[0] < (1 - 0.1) * image.size[0]: # put text margin here
-            font_size += 1
-            font = ImageFont.truetype(self._font_filepath, font_size)
-
+        font = ImageFont.truetype(self._font_filepath, self.__calculate_font_size(insult, image))
         text_pos_x = 5
         text_pos_y = int(image_height * 0.75)
         draw.text(xy=(text_pos_x, text_pos_y), text=insult, fill=(225, 69, 0), font=font)
-
         image.save(self._completed_meme_path)
